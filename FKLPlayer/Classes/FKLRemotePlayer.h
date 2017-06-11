1da8fde687d1245d575b4278577234223769596a
kun <kun@kundeMacBook-Air.local>
2017-06-11 12:23:35 +0800

    事件&数据提供

--- FKLPlayer/Classes/FKLRemotePlayer.h
+++ FKLPlayer/Classes/FKLRemotePlayer.h
@@ -8,6 +8,20 @@
 
 #import <Foundation/Foundation.h>
 
+/**
+ *  播放器的状态
+ *  因为UI界面需要加载状态显示，所以需要提供加载状态
+ *
+ */
+typedef NS_ENUM(NSInteger, FKLRemotePlayerState) {
+    FKLRemotePlayerStateUnknown = 0,
+    FKLRemotePlayerStateLoading = 1,
+    FKLRemotePlayerStatePlaying = 2,
+    FKLRemotePlayerStateStopped = 3,
+    FKLRemotePlayerStatePause   = 4,
+    FKLRemotePlayerStateFailed  = 5
+};
+
 @interface FKLRemotePlayer : NSObject
 
 + (instancetype)shareInstance;
@@ -21,10 +35,26 @@
 - (void)seekWithTimeDiffer:(NSTimeInterval)timeDiffer;
 - (void)seekWithProgress:(float)progress;
 
-- (void)setRate:(float)rate;
+//- (void)setRate:(float)rate;
 
-- (void)setMuted:(BOOL)muted;
+//- (void)setMuted:(BOOL)muted;
 
-- (void)setVolume:(float)volume;
+//- (void)setVolume:(float)volume;
+
+#pragma mark - 数据源
+
+@property (nonatomic, assign) BOOL muted;
+@property (nonatomic, assign) float volume;
+@property (nonatomic, assign) float rate;
+
+@property (nonatomic, assign, readonly) NSTimeInterval totalTime;
+@property (nonatomic, copy, readonly) NSString *totalTimeFormat;
+@property (nonatomic, assign, readonly) NSTimeInterval currentTime;
+@property (nonatomic, copy, readonly) NSString *currentTimeFormat;
+@property (nonatomic, assign, readonly) float progress;
+@property (nonatomic, strong, readonly) NSURL *url;
+@property (nonatomic, assign, readonly) float loadDataProgress;
+
+@property (nonatomic, assign,readonly) FKLRemotePlayerState state;
 
 @end
--- FKLPlayer/Classes/FKLRemotePlayer.m
+++ FKLPlayer/Classes/FKLRemotePlayer.m
@@ -9,7 +9,9 @@
 #import "FKLRemotePlayer.h"
 #import <AVFoundation/AVFoundation.h>
 
-@interface FKLRemotePlayer ()
+@interface FKLRemotePlayer () {
+    BOOL _isUserPause;
+}
 
 @property (nonatomic, strong) AVPlayer *player;
 
@@ -43,7 +45,17 @@ static FKLRemotePlayer *_shareInstance;
     
 //    AVPlayer *player = [AVPlayer playerWithURL:url];
 //    [player play];
+    NSURL *currentUrl = [(AVURLAsset *)self.player.currentItem.asset URL];
+    if ( [url isEqual:currentUrl] ) {
+        NSLog(@"当前播放任务已经存在");
+        [self resume];
+        return;
+    }
     
+    if ( self.player.currentItem ) {
+        [self removeOberver];
+    }
+    _url = url;
     // 1，资源的请求
     AVAsset *asset = [AVAsset assetWithURL:url];
     
@@ -52,6 +64,9 @@ static FKLRemotePlayer *_shareInstance;
     // 当资源的组织者，告诉我们资源准备好了之后，我们再播放
     // AVPlayerItemStatus status    KVO
     [item addObserver:self forKeyPath:@"status" options:NSKeyValueObservingOptionNew context:nil];
+    [item addObserver:self forKeyPath:@"playbackLikelyToKeepUp" options:NSKeyValueObservingOptionNew context:nil];
+    [[NSNotificationCenter defaultCenter] addObserver:self selector:@selector(playEnd) name:AVPlayerItemDidPlayToEndTimeNotification object:nil];
+    [[NSNotificationCenter defaultCenter] addObserver:self selector:@selector(playInterupt) name:AVPlayerItemPlaybackStalledNotification object:nil];
     
     // 3，资源的播放
     self.player = [AVPlayer playerWithPlayerItem:item];
@@ -59,25 +74,34 @@ static FKLRemotePlayer *_shareInstance;
 
 - (void)pause {
     [self.player pause];
+    _isUserPause = YES;
+    if ( self.player ) {
+        self.state = FKLRemotePlayerStatePause;
+    }
 }
 - (void)resume {
     [self.player play];
+    _isUserPause = NO;
+    if ( self.player && self.player.currentItem.playbackLikelyToKeepUp ) {
+        self.state = FKLRemotePlayerStatePlaying;
+    }
+    
 }
 - (void)stop {
     [self.player pause];
     self.player = nil;
+    if ( self.player ) {
+        self.state = FKLRemotePlayerStateStopped;
+    }
 }
 
 - (void)seekWithTimeDiffer:(NSTimeInterval)timeDiffer {
     // 当前音频资源的总时长
-    CMTime totalTime = self.player.currentItem.duration;
-    NSTimeInterval totalTimeSec = CMTimeGetSeconds(totalTime);
     // 当前资源，已经播放的时长
-    CMTime playTime = self.player.currentItem.currentTime;
-    NSTimeInterval playTimeSec = CMTimeGetSeconds(playTime);
+    NSTimeInterval playTimeSec = [self currentTime];
     playTimeSec += timeDiffer;
     
-    [self seekWithTimeDiffer:playTimeSec / totalTimeSec];
+    [self seekWithTimeDiffer:playTimeSec / [self totalTime]];
 }
 - (void)seekWithProgress:(float)progress {
     /**
@@ -92,8 +116,7 @@ static FKLRemotePlayer *_shareInstance;
         return;
     }
     
-    CMTime totalTime = self.player.currentItem.duration;
-    NSTimeInterval totalSec = CMTimeGetSeconds(totalTime);
+    NSTimeInterval totalSec = [self totalTime];
     NSTimeInterval playTimeSec = totalSec * progress;
     CMTime currentTime = CMTimeMake(playTimeSec, 1);
     [self.player seekToTime:currentTime completionHandler:^(BOOL finished) {
@@ -110,8 +133,16 @@ static FKLRemotePlayer *_shareInstance;
     [self.player setRate:rate];
 }
 
+- (float)rate {
+    return self.player.rate; 
+}
+
 - (void)setMuted:(BOOL)muted {
     [self.player setMuted:muted];
+}
+
+- (BOOL)muted {
+    return self.player.muted;
 }
 
 - (void)setVolume:(float)volume {
@@ -124,6 +155,10 @@ static FKLRemotePlayer *_shareInstance;
     [self.player setVolume:volume];
 }
 
+- (float)volume {
+    return self.player.volume;
+}
+
 #pragma mark - KVO
 
 - (void)observeValueForKeyPath:(NSString *)keyPath ofObject:(id)object change:(NSDictionary<NSKeyValueChangeKey,id> *)change context:(void *)context {
@@ -131,11 +166,84 @@ static FKLRemotePlayer *_shareInstance;
         AVPlayerItemStatus status = [change[NSKeyValueChangeNewKey] integerValue];
         if ( status == AVPlayerItemStatusReadyToPlay ) {
             NSLog(@"资源准备好了，这时候播放就没有问题");
-            [self.player play];
+            [self resume];
         } else {
             NSLog(@"状态未知");
+            self.state = FKLRemotePlayerStateUnknown;
+        }
+    } else if ( [@"playbackLikelyToKeepUp" isEqualToString:keyPath] ) {
+        BOOL ptk = [change[NSKeyValueChangeNewKey] boolValue];
+        if ( ptk ) {
+            NSLog(@"当前的资源，准备的已经足够播放了");
+            if ( !_isUserPause ) {
+                [self resume];
+            }
+        } else {
+            NSLog(@"当前的资源还不够，正在加载过程当中");
+            self.state = FKLRemotePlayerStateLoading;
         }
     }
+}
+
+- (void)removeOberver {
+    [self.player.currentItem removeObserver:self forKeyPath:@"status"];
+    [self.player.currentItem removeObserver:self forKeyPath:@"playbackLikelyToKeepUp"];
+}
+
+#pragma mark - 数据／事件
+
+- (NSTimeInterval)totalTime {
+    CMTime totalTime = self.player.currentItem.duration;
+    NSTimeInterval totalTimeSec = CMTimeGetSeconds(totalTime);
+    return totalTimeSec;
+}
+
+- (NSString *)totalTimeFormat {
+    return [NSString stringWithFormat:@"%02zd:%02zd", (int)self.totalTime / 60, (int)self.totalTime % 60];
+}
+
+- (NSTimeInterval)currentTime {
+    CMTime playTime = self.player.currentItem.currentTime;
+    NSTimeInterval playTimeSec = CMTimeGetSeconds(playTime);
+    if ( isnan(playTimeSec) ) {
+        return 0;
+    }
+    return playTimeSec;
+}
+
+- (NSString *)currentTimeFormat {
+    return [NSString stringWithFormat:@"%02zd:%02zd", (int)self.currentTime / 60, (int)self.currentTime % 60];
+}
+
+- (float)progress {
+    if ( 0 == self.totalTime ) {
+        return 0;
+    }
+    return self.currentTime / self.totalTime;
+}
+
+- (float)loadDataProgress {
+    if ( 0 == self.totalTime ) {
+        return 0;
+    }
+    CMTimeRange timeRange = [[self.player.currentItem loadedTimeRanges].lastObject CMTimeRangeValue];
+    CMTime loadTime = CMTimeAdd(timeRange.start, timeRange.duration);
+    NSTimeInterval loadTimeSec = CMTimeGetSeconds(loadTime);
+    return loadTimeSec / self.totalTime;
+}
+
+- (void)setState:(FKLRemotePlayerState)state {
+    _state = state;
+}
+
+- (void)playEnd {
+    NSLog(@"播放完成");
+    self.state = FKLRemotePlayerStateStopped;
+}
+
+- (void)playInterupt {
+    NSLog(@"播放被打断");
+    self.state = FKLRemotePlayerStatePause;
 }
 
 @end
